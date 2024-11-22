apache/spark

Introduce ScopedExpression to control scope of expression resolution

sql/core/src/main/scala/org/apache/spark/sql/KeyValueGroupedDataset.scala
@@ -20,10 +20,11 @@ package org.apache.spark.sql
 import org.apache.spark.api.java.function._
 import org.apache.spark.sql.catalyst.analysis.{EliminateEventTimeWatermark, UnresolvedAttribute}
 import org.apache.spark.sql.catalyst.encoders.{encoderFor, ExpressionEncoder}
-import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.catalyst.expressions.{Attribute, ScopedExpression}
 import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.execution.QueryExecution
 import org.apache.spark.sql.expressions.ReduceAggregator
+import org.apache.spark.sql.internal.ExpressionColumnNode
 import org.apache.spark.sql.internal.TypedAggUtils.{aggKeyColumn, withInputType}
 import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, OutputMode, StatefulProcessor, StatefulProcessorWithInitialState, TimeMode}
 
@@ -305,7 +306,12 @@ class KeyValueGroupedDataset[K, V] private[sql](
   /** @inheritdoc */
   protected def aggUntyped(columns: TypedColumn[_, _]*): Dataset[_] = {
     val encoders = columns.map(c => encoderFor(c.encoder))
-    val namedColumns = columns.map(c => withInputType(c.named, vExprEnc, dataAttributes))
+    val namedColumns =
+      columns
+        // SPARK-42199: resolve these aggregate expressions only against dataAttributes
+        // this is to hide key column from expression resolution
+        .map(scopeTypedColumn(dataAttributes))
+        .map(c => withInputType(c.named, vExprEnc, dataAttributes))
     val keyColumn = aggKeyColumn(kExprEnc, groupingAttributes)
     val aggregate = Aggregate(groupingAttributes, keyColumn +: namedColumns, logicalPlan)
     val execution = new QueryExecution(sparkSession, aggregate)
@@ -333,6 +339,11 @@ class KeyValueGroupedDataset[K, V] private[sql](
         this.logicalPlan,
         other.logicalPlan))
   }
+
+  private def scopeTypedColumn(
+      scope: Seq[Attribute])(
+      typedCol: TypedColumn[_, _]): TypedColumn[_, _] =
+    new TypedColumn(ExpressionColumnNode(ScopedExpression(typedCol.expr, scope)), typedCol.encoder)
 
   override def toString: String = {
     val builder = new StringBuilder

sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
@@ -567,6 +567,68 @@ class DatasetSuite extends QueryTest
       (1, 1))
   }
 
+  test("SPARK-42199: groupBy function, agg, unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).agg(count("unknown"))
+      },
+      errorClass = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`id`"),
+      context = ExpectedContext(fragment = "count", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, flatMapSortedGroups, unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).flatMapSortedGroups($"unknown") {
+          case (g, it) => Iterator((g, it.mkString(", ")))
+        }
+      },
+      errorClass = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`id`"),
+      context = ExpectedContext(fragment = "$", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, mapValues, agg, unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).mapValues(id => id).agg(count("unknown"))
+      },
+      errorClass = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`value`"),
+      context = ExpectedContext(fragment = "count", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, mapValues, flatMapSortedGroups, " +
+    "unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).mapValues(id => id).flatMapSortedGroups($"unknown") {
+          case (g, it) => Iterator((g, it.mkString(", ")))
+        }
+      },
+      errorClass = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`value`"),
+      context = ExpectedContext(fragment = "$", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, agg expr resolution") {
+    val actual = spark.range(3)
+      .withColumnRenamed("id", "value").as[Long] // add column 'value' to dataset
+      .groupByKey(value => value * 2) // produces key column 'value'
+      .agg(sum("value").as[Long]) // 'value' does not resolve to key column
+      .collect()
+    assert(actual.sorted === Seq((0, 0), (2, 1), (4, 2)))
+
+    val actual2 = spark.range(3)
+      .withColumnRenamed("id", "value").as[Long] // add column 'value' to dataset
+      .groupByKey(value => value * 2) // produces key column 'value'
+      .mapValues(value => value * -1) // replaces value column 'value'
+      .agg(sum("value").as[Long]) // 'value' does not resolve to key column
+      .collect()
+    assert(actual2.sorted === Seq((0, 0), (2, -1), (4, -2)))
+  }
+
   test("groupBy function, map") {
     val ds = Seq(("a", 10), ("a", 20), ("b", 1), ("b", 2), ("c", 1)).toDS()
     val grouped = ds.groupByKey(v => (v._1, "word"))
@@ -698,6 +760,42 @@ class DatasetSuite extends QueryTest
       .keys
       .collect()
     assert(result.sortBy(_.a) === Seq(K1(0), K1(0), K1(1), K1(1)))
+  }
+
+  test("SPARK-42199: groupBy function, flatMapSortedGroups expr resolution") {
+    val ds = Seq(("a", 1, 10), ("a", 2, 20), ("b", 2, 1), ("b", 1, 2), ("c", 1, 1))
+      .toDF("key", "seq", "value")
+
+    // groupByKey produces key column 'value'
+    val grouped = ds.groupByKey(v => v.getString(0))
+    // 'value' does not resolve to key column
+    val aggregated = grouped.flatMapSortedGroups($"value") {
+      (g, iter) => Iterator(g, iter.mkString(", "))
+    }
+
+    checkDatasetUnorderly(
+      aggregated,
+      "a", "[a,1,10], [a,2,20]",
+      "b", "[b,2,1], [b,1,2]",
+      "c", "[c,1,1]"
+    )
+
+
+    // groupByKey produces key column 'value'
+    val grouped2 = ds.groupByKey(v => v.getString(0))
+      // mapValues replaces value column 'value'
+      .mapValues(v => v.getInt(1) * -1)
+    // 'value' does not resolve to key column
+    val aggregated2 = grouped2.flatMapSortedGroups($"value") {
+      (g, iter) => Iterator(g, iter.mkString(", "))
+    }
+
+    checkDatasetUnorderly(
+      aggregated2,
+      "a", "-2, -1",
+      "b", "-2, -1",
+      "c", "-1"
+    )
   }
 
   test("groupBy function, mapValues, flatMap") {
@@ -970,6 +1068,44 @@ class DatasetSuite extends QueryTest
     )
     val joined = rhs.join(cogrouped, col("ID") === col("value"), "left")
     checkAnswer(joined, Row(0L, 123L, 0L) :: Nil)
+  }
+
+  test("SPARK-42199: cogroup sorted, sort expr resolution") {
+    val left = Seq(1 -> "a", 3 -> "xyz", 5 -> "hello", 3 -> "abcx", 3 -> "ijk").toDF("id", "value")
+    val right = Seq(2 -> "q", 3 -> "w", 5 -> "x", 5 -> "z", 3 -> "a", 5 -> "y").toDF("id", "value")
+
+    // dataset column 'value' coexists with key column 'value' produced by groupByKey
+    val groupedLeft = left.groupByKey(_.getInt(0))
+    val groupedRight = right.groupByKey(_.getInt(0))
+
+    // 'value' does not resolve to key column
+    val actual = groupedLeft.cogroupSorted(groupedRight)($"value")($"value".desc) {
+      (key, left, right) => Iterator(key -> (left.mkString + "#" + right.mkString))
+    }
+    checkDatasetUnorderly(
+      actual,
+      1 -> "[1,a]#",
+      2 -> "#[2,q]",
+      3 -> "[3,abcx][3,ijk][3,xyz]#[3,w][3,a]",
+      5 -> "[5,hello]#[5,z][5,y][5,x]")
+
+
+    val groupedLeft2 = groupedLeft
+      // mapValues replaces value column 'value'
+      .mapValues(_.getString(1).reverse)
+    val groupedRight2 = groupedRight
+      // mapValues replaces value column 'value'
+      .mapValues(_.getString(1).reverse)
+    // 'value' does not resolve to key column
+    val actual2 = groupedLeft2.cogroupSorted(groupedRight2)($"value")($"value".desc) {
+      (key, left, right) => Iterator(key -> (left.mkString + "#" + right.mkString))
+    }
+    checkDatasetUnorderly(
+      actual2,
+      1 -> "a#",
+      2 -> "#q",
+      3 -> "kjixcbazyx#wa",
+      5 -> "olleh#zyx")
   }
 
   test("SPARK-34806: observation on datasets") {

waleednn/apache-spark

sql/core/src/main/scala/org/apache/spark/sql/KeyValueGroupedDataset.scala
@@ -21,11 +21,12 @@ import org.apache.spark.api.java.function._
 import org.apache.spark.sql.catalyst.analysis.{EliminateEventTimeWatermark, UnresolvedAttribute}
 import org.apache.spark.sql.catalyst.encoders.AgnosticEncoders.{agnosticEncoderFor, ProductEncoder}
 import org.apache.spark.sql.catalyst.encoders.encoderFor
-import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.catalyst.expressions.{Attribute, ScopedExpression}
 import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.classic.ClassicConversions._
 import org.apache.spark.sql.execution.QueryExecution
 import org.apache.spark.sql.expressions.ReduceAggregator
+import org.apache.spark.sql.internal.ExpressionColumnNode
 import org.apache.spark.sql.internal.TypedAggUtils.{aggKeyColumn, withInputType}
 import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, OutputMode, StatefulProcessor, StatefulProcessorWithInitialState, TimeMode}
 
@@ -307,9 +308,12 @@ class KeyValueGroupedDataset[K, V] private[sql](
     val keyAgEncoder = agnosticEncoderFor(kEncoder)
     val valueExprEncoder = encoderFor(vEncoder)
     val encoders = columns.map(c => agnosticEncoderFor(c.encoder))
-    val namedColumns = columns.map { c =>
-      withInputType(c.named, valueExprEncoder, dataAttributes)
-    }
+    val namedColumns =
+      columns
+        // SPARK-42199: resolve these aggregate expressions only against dataAttributes
+        // this is to hide key column from expression resolution
+        .map(scopeTypedColumn(dataAttributes))
+        .map(c => withInputType(c.named, valueExprEncoder, dataAttributes))
     val keyColumn = aggKeyColumn(keyAgEncoder, groupingAttributes)
     val aggregate = Aggregate(groupingAttributes, keyColumn +: namedColumns, logicalPlan)
     new Dataset(sparkSession, aggregate, ProductEncoder.tuple(keyAgEncoder +: encoders))
@@ -335,6 +339,11 @@ class KeyValueGroupedDataset[K, V] private[sql](
         this.logicalPlan,
         other.logicalPlan))
   }
+
+  private def scopeTypedColumn(
+      scope: Seq[Attribute])(
+      typedCol: TypedColumn[_, _]): TypedColumn[_, _] =
+    new TypedColumn(ExpressionColumnNode(ScopedExpression(typedCol.expr, scope)), typedCol.encoder)
 
   override def toString: String = {
     val builder = new StringBuilder

sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
@@ -567,6 +567,68 @@ class DatasetSuite extends QueryTest
       (1, 1))
   }
 
+  test("SPARK-42199: groupBy function, agg, unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).agg(count("unknown"))
+      },
+      condition = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`id`"),
+      context = ExpectedContext(fragment = "count", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, flatMapSortedGroups, unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).flatMapSortedGroups($"unknown") {
+          case (g, it) => Iterator((g, it.mkString(", ")))
+        }
+      },
+      condition = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`id`"),
+      context = ExpectedContext(fragment = "$", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, mapValues, agg, unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).mapValues(id => id).agg(count("unknown"))
+      },
+      condition = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`value`"),
+      context = ExpectedContext(fragment = "count", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, mapValues, flatMapSortedGroups, " +
+    "unresolved reference suggestions") {
+    checkError(
+      exception = intercept[AnalysisException] {
+        spark.range(10).groupByKey(id => id).mapValues(id => id).flatMapSortedGroups($"unknown") {
+          case (g, it) => Iterator((g, it.mkString(", ")))
+        }
+      },
+      condition = "UNRESOLVED_COLUMN.WITH_SUGGESTION",
+      parameters = Map("objectName" -> "`unknown`", "proposal" -> "`value`"),
+      context = ExpectedContext(fragment = "$", getCurrentClassCallSitePattern))
+  }
+
+  test("SPARK-42199: groupBy function, agg expr resolution") {
+    val actual = spark.range(3)
+      .withColumnRenamed("id", "value").as[Long] // add column 'value' to dataset
+      .groupByKey(value => value * 2) // produces key column 'value'
+      .agg(sum("value").as[Long]) // 'value' does not resolve to key column
+      .collect()
+    assert(actual.sorted === Seq((0, 0), (2, 1), (4, 2)))
+
+    val actual2 = spark.range(3)
+      .withColumnRenamed("id", "value").as[Long] // add column 'value' to dataset
+      .groupByKey(value => value * 2) // produces key column 'value'
+      .mapValues(value => value * -1) // replaces value column 'value'
+      .agg(sum("value").as[Long]) // 'value' does not resolve to key column
+      .collect()
+    assert(actual2.sorted === Seq((0, 0), (2, -1), (4, -2)))
+  }
+
   test("groupBy function, map") {
     val ds = Seq(("a", 10), ("a", 20), ("b", 1), ("b", 2), ("c", 1)).toDS()
     val grouped = ds.groupByKey(v => (v._1, "word"))
@@ -698,6 +760,42 @@ class DatasetSuite extends QueryTest
       .keys
       .collect()
     assert(result.sortBy(_.a) === Seq(K1(0), K1(0), K1(1), K1(1)))
+  }
+
+  test("SPARK-42199: groupBy function, flatMapSortedGroups expr resolution") {
+    val ds = Seq(("a", 1, 10), ("a", 2, 20), ("b", 2, 1), ("b", 1, 2), ("c", 1, 1))
+      .toDF("key", "seq", "value")
+
+    // groupByKey produces key column 'value'
+    val grouped = ds.groupByKey(v => v.getString(0))
+    // 'value' does not resolve to key column
+    val aggregated = grouped.flatMapSortedGroups($"value") {
+      (g, iter) => Iterator(g, iter.mkString(", "))
+    }
+
+    checkDatasetUnorderly(
+      aggregated,
+      "a", "[a,1,10], [a,2,20]",
+      "b", "[b,2,1], [b,1,2]",
+      "c", "[c,1,1]"
+    )
+
+
+    // groupByKey produces key column 'value'
+    val grouped2 = ds.groupByKey(v => v.getString(0))
+      // mapValues replaces value column 'value'
+      .mapValues(v => v.getInt(1) * -1)
+    // 'value' does not resolve to key column
+    val aggregated2 = grouped2.flatMapSortedGroups($"value") {
+      (g, iter) => Iterator(g, iter.mkString(", "))
+    }
+
+    checkDatasetUnorderly(
+      aggregated2,
+      "a", "-2, -1",
+      "b", "-2, -1",
+      "c", "-1"
+    )
   }
 
   test("groupBy function, mapValues, flatMap") {
@@ -970,6 +1068,44 @@ class DatasetSuite extends QueryTest
     )
     val joined = rhs.join(cogrouped, col("ID") === col("value"), "left")
     checkAnswer(joined, Row(0L, 123L, 0L) :: Nil)
+  }
+
+  test("SPARK-42199: cogroup sorted, sort expr resolution") {
+    val left = Seq(1 -> "a", 3 -> "xyz", 5 -> "hello", 3 -> "abcx", 3 -> "ijk").toDF("id", "value")
+    val right = Seq(2 -> "q", 3 -> "w", 5 -> "x", 5 -> "z", 3 -> "a", 5 -> "y").toDF("id", "value")
+
+    // dataset column 'value' coexists with key column 'value' produced by groupByKey
+    val groupedLeft = left.groupByKey(_.getInt(0))
+    val groupedRight = right.groupByKey(_.getInt(0))
+
+    // 'value' does not resolve to key column
+    val actual = groupedLeft.cogroupSorted(groupedRight)($"value")($"value".desc) {
+      (key, left, right) => Iterator(key -> (left.mkString + "#" + right.mkString))
+    }
+    checkDatasetUnorderly(
+      actual,
+      1 -> "[1,a]#",
+      2 -> "#[2,q]",
+      3 -> "[3,abcx][3,ijk][3,xyz]#[3,w][3,a]",
+      5 -> "[5,hello]#[5,z][5,y][5,x]")
+
+
+    val groupedLeft2 = groupedLeft
+      // mapValues replaces value column 'value'
+      .mapValues(_.getString(1).reverse)
+    val groupedRight2 = groupedRight
+      // mapValues replaces value column 'value'
+      .mapValues(_.getString(1).reverse)
+    // 'value' does not resolve to key column
+    val actual2 = groupedLeft2.cogroupSorted(groupedRight2)($"value")($"value".desc) {
+      (key, left, right) => Iterator(key -> (left.mkString + "#" + right.mkString))
+    }
+    checkDatasetUnorderly(
+      actual2,
+      1 -> "a#",
+      2 -> "#q",
+      3 -> "kjixcbazyx#wa",
+      5 -> "olleh#zyx")
   }
 
   test("SPARK-34806: observation on datasets") {